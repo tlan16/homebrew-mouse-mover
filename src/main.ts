a458cbab185fa07d85c72d96dce6abc58c9b12d7
Frank Lan <frank.lan@rea-group.com>
2024-06-21 14:42:41 +1000

    idle detechtion

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,50 @@ import robot from "robotjs";
 import sleepSynchronously from "sleep-synchronously";
 import { getRandomInt } from "./getRandomInt";
 
+const idleMargin = 100;
+const idleDetectionDelay = 1e3;
+const idleThreshold = 4;
+
 // Speed up the mouse.
 robot.setMouseDelay(2);
 
-const main = (offsetX = getRandomInt(), offsetY = getRandomInt()) => {
+const main = ({
+  previousX,
+  previousY,
+  idleCounter = 0,
+}: {
+  previousX?: number;
+  previousY?: number;
+  idleCounter?: number;
+} = {}) => {
+  const { x, y } = robot.getMousePos();
+  const gap = {
+    x: Math.abs((previousX ?? x) - x),
+    y: Math.abs((previousY ?? y) - y),
+  };
+  // console.debug({
+  //   previousX,
+  //   previousY,
+  //   idleCounter,
+  //   gap,
+  // });
+  if (gap.x > idleMargin || gap.y > idleMargin) {
+    sleepSynchronously( idleDetectionDelay);
+    main({ previousX: x, previousY: y, idleCounter: 0 });
+    return;
+  }
+  if (idleCounter < idleThreshold) {
+    sleepSynchronously( idleDetectionDelay);
+    main({ previousX: x, previousY: y, idleCounter: idleCounter + 1 });
+    return;
+  }
+
   robot.moveMouseSmooth(
-    robot.getMousePos().x + offsetX,
-    robot.getMousePos().y + offsetY,
+    (previousX ?? robot.getMousePos().x) + getRandomInt(),
+    (previousY ?? robot.getMousePos().y) + getRandomInt(),
   );
-  sleepSynchronously(getRandomInt(1e3, 5e3));
-  main(-offsetX + getRandomInt(), -offsetY + getRandomInt());
+  sleepSynchronously( getRandomInt(idleDetectionDelay, 5e3));
+  main({ previousX: x, previousY: y, idleCounter: idleCounter });
 };
 
 main();
