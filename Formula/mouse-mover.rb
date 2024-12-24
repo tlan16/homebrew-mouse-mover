75f0294617f4230aa441ce2b8d1e2f6a0641aa8e
Frank Lan <frank.lan@rea-group.com>
2024-12-24 16:42:49 +1100

    version 1.0.1

--- Formula/mouse-mover.rb
+++ Formula/mouse-mover.rb
@@ -2,7 +2,7 @@ class MouseMover < Formula
   desc "Mouse mover utility"
   homepage "https://github.com/tlan16/mouse-mover"
   url "https://github.com/tlan16/homebrew-mouse-mover.git", using: :git, branch: "main"
-  version "1.0.0"
+  version "1.0.1"
 
   depends_on "node@22" => :build
   depends_on "pnpm" => :build
