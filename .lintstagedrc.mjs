7725cebc403bf5d6a49c4e05b6894e5984e0dbef
Frank Lan <franklan118@gmail.com>
2025-08-08 09:40:49 +1000

    fix stages

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -5,10 +5,8 @@ const config = {
   "*.ts": "eslint --fix",
   "*.js": "eslint --fix",
   "*.md": "bun prettier --write",
-  "*.sh": "bun shellcheck",
   "*.yaml": "yamllint",
   "*.yml": "yamllint",
-  ".husky/pre-commit": "bun shellcheck",
 }
 
 export default config
